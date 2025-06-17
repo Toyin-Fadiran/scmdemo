7e81dee1808613c2ef4265ec2d763333ce6a83f6
Toyin Fadiran <Oluwatoyin.Fadiran.Contractor@pepsico.com>
2025-06-16 22:57:19 -0500

    push demo scm app

---
+++ src/test/java/com/example/scmdemo/ScmdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
